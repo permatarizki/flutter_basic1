8239787c5b625b730c0faac9a03c0bdaa1c5d13f
Permata NMR <permatarizki@gmail.com>
2021-01-06 11:16:42 +0700

    add Raised Button n text in center screen

--- simplelistcrud/lib/main.dart
+++ simplelistcrud/lib/main.dart
@@ -11,7 +11,16 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
       appBar: AppBar(title: Text('List CRUD')),
       body: Center(
-        child: Text('HELLO WORLD'),
+        child: Column(
+          children: [
+            RaisedButton(
+                child: Text('Button'),
+                onPressed: () {},
+                color: Color.fromARGB(255, 66, 165, 245)),
+            Text('HELLO WORLD'),
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
       ),
     ));
   }
